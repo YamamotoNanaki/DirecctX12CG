b1f964df5897d873c9298f6b68e42257c7e6aeea
LC1A_21_Yamamoto_Nanaki <k021g1320@g.neec.ac.jp>
2022-04-13 12:22:28 +0900

    01_02

--- main.cpp
+++ main.cpp
@@ -1,4 +1,20 @@
 #include <Windows.h>
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <vector>
+#include <string>
+#include <DirectXMath.h>
+#include <d3dcompiler.h>
+#include <dinput.h>
+
+using namespace DirectX;
+using namespace std;
+
+#pragma comment(lib,"d3d12.lib") 
+#pragma comment(lib,"dxgi.lib")
+#pragma comment(lib,"d3dcompiler.lib")
+#pragma comment(lib,"dinput8.lib")
+#pragma comment(lib,"dxguid.lib")
 
 //ウィンドウプロシージャ
 LRESULT WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
@@ -53,7 +69,159 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 
 	MSG msg{}; // メッセージ
 
+#pragma region デバッグ
+#ifdef _DEBUG
+// デバックレイヤーをオンに
+	ID3D12Debug* debugController;
+	if (SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(&debugController))))
+	{
+		debugController->EnableDebugLayer();
+	}
+#endif
+#pragma endregion デバッグ
+
 #pragma region DirectX初期化処理
+
+	#pragma region DirectX初期化
+	HRESULT result;
+	ID3D12Device* device = nullptr;
+	IDXGIFactory6* dxgiFactory = nullptr;
+	IDXGISwapChain4* swapchain = nullptr;
+	ID3D12CommandAllocator* commandAllocator = nullptr;
+	ID3D12GraphicsCommandList* commandList = nullptr;
+	ID3D12CommandQueue* cmdQueue = nullptr;
+	ID3D12DescriptorHeap* rtvHeaps = nullptr;
+#pragma endregion DirectX初期化
+
+	#pragma region グラフィックスボードのアダプタを列挙
+	// DXGIファクトリーの生成
+	result = CreateDXGIFactory1(IID_PPV_ARGS(&dxgiFactory));
+	// アダプターの列挙用
+	vector<IDXGIAdapter4*> adapters;
+	// ここに特定の名前を持アダプターオブジェクトを入れる
+	IDXGIAdapter4* tmpAdapter = nullptr;
+	for (UINT i = 0; dxgiFactory->EnumAdapterByGpuPreference(i, DXGI_GPU_PREFERENCE_HIGH_PERFORMANCE,IID_PPV_ARGS(&tmpAdapter)) != DXGI_ERROR_NOT_FOUND; i++)
+	{
+		adapters.push_back(tmpAdapter); //動的配列に追加する
+	}
+
+	for (size_t i = 0; i < adapters.size(); i++)
+	{
+		DXGI_ADAPTER_DESC3 adapterDesc;
+		adapters[i]->GetDesc3(&adapterDesc); // アダプターの情報を取得
+
+		// ソフトウェアデバイスを回避
+		if (!(adapterDesc.Flags & DXGI_ADAPTER_FLAG3_SOFTWARE))
+		{
+			tmpAdapter = adapters[i]; //採用
+			break;
+		}
+	}
+#pragma endregion グラフィックスボードのアダプタを列挙
+
+	#pragma region デバイスの生成
+	// 対応レベルの配列
+	D3D_FEATURE_LEVEL levels[] =
+	{
+		D3D_FEATURE_LEVEL_12_1,
+		D3D_FEATURE_LEVEL_12_0,
+		D3D_FEATURE_LEVEL_11_1,
+		D3D_FEATURE_LEVEL_11_0,
+	};
+
+	D3D_FEATURE_LEVEL featureLevel;
+
+	for (size_t i = 0; i < _countof(levels); i++)
+	{
+		// 採用したアダプターでデバイスを生成
+		result = D3D12CreateDevice(tmpAdapter, levels[i], IID_PPV_ARGS(&device));
+		if (result == S_OK)
+		{
+			// デバイスを生成できた時点でループを抜ける
+			featureLevel = levels[i];
+			break;
+		}
+	}
+
+#pragma endregion デバイスの生成
+
+	#pragma region コマンドリスト
+	// コマンドアロケータを生成
+	result = device->CreateCommandAllocator(
+		D3D12_COMMAND_LIST_TYPE_DIRECT,
+		IID_PPV_ARGS(&commandAllocator));
+	assert(SUCCEEDED(result));
+
+	// コマンドリストの生成
+	result = device->CreateCommandList(0,
+		D3D12_COMMAND_LIST_TYPE_DIRECT,
+		commandAllocator, nullptr,
+		IID_PPV_ARGS(&commandList));
+	assert(SUCCEEDED(result));
+#pragma endregion コマンドリスト
+
+	#pragma region コマンドキュー
+	// 標準設定でコマンドキューを生成
+	D3D12_COMMAND_QUEUE_DESC commandQueueDesc{};
+
+	device->CreateCommandQueue(&commandQueueDesc, IID_PPV_ARGS(&cmdQueue));
+	assert(SUCCEEDED(result));
+#pragma endregion コマンドキュー
+
+	#pragma region スワップチェーンの生成
+	// 各種設定をしてスワップチェーンを生成
+	DXGI_SWAP_CHAIN_DESC1 swapChainDesc{};
+	swapChainDesc.Width = window_width;
+	swapChainDesc.Height = window_height;
+	swapChainDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM; // 色情報の書式
+	swapChainDesc.SampleDesc.Count = 1; // マルチサンプルしない
+	swapChainDesc.BufferUsage = DXGI_USAGE_BACK_BUFFER; // バックバッファ用
+	swapChainDesc.BufferCount = 2; // バッファ数を２つに設定
+	swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD; //フリップ後は破壊
+	swapChainDesc.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
+
+	result = dxgiFactory->CreateSwapChainForHwnd(
+		cmdQueue, hwnd, &swapChainDesc, nullptr, nullptr, (IDXGISwapChain1**)&swapchain);
+#pragma endregion スワップチェーンの生成
+
+	#pragma region デスクリプタヒープの生成
+	// 各種設定をしてデスクリプタヒープを生成
+	D3D12_DESCRIPTOR_HEAP_DESC rtvHeapDesc{};
+	rtvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV; // レンダーターゲットビュー
+	rtvHeapDesc.NumDescriptors = swapChainDesc.BufferCount;	// 裏表の２つ
+	device->CreateDescriptorHeap(&rtvHeapDesc, IID_PPV_ARGS(&rtvHeaps));
+#pragma endregion デスクリプタヒープの生成
+
+	#pragma region レンダーターゲットビューの生成
+	// 裏表の2つ分について
+	vector<ID3D12Resource*> backBuffers(2);
+	backBuffers.resize(swapChainDesc.BufferCount);
+
+	for (size_t i = 0; i < backBuffers.size(); i++)
+	{
+		// スワップチェーンからバッファを取得
+		swapchain->GetBuffer((UINT)i, IID_PPV_ARGS(&backBuffers[i]));
+		// デスクリプタヒープのハンドルの取得
+		D3D12_CPU_DESCRIPTOR_HANDLE rtvHandle = rtvHeaps->GetCPUDescriptorHandleForHeapStart();
+		// 裏か表かアドレスがずれる
+		rtvHandle.ptr += i * device->GetDescriptorHandleIncrementSize(rtvHeapDesc.Type);
+		D3D12_RENDER_TARGET_VIEW_DESC rtvDesc{};
+		rtvDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM_SRGB;
+		rtvDesc.ViewDimension = D3D12_RTV_DIMENSION_TEXTURE2D;
+		// レンダーターゲットビューの生成
+		device->CreateRenderTargetView(backBuffers[i], &rtvDesc, rtvHandle);
+	}
+#pragma endregion レンダーターゲットビューの生成
+
+	#pragma region フェンスの生成
+	// フェンスの生成
+	ID3D12Fence* fence = nullptr;
+	UINT64 fenceVal = 0;
+
+	result = device->CreateFence(fenceVal, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&fence));
+#pragma endregion フェンスの生成
+
+	// DirectX初期化処理 ここまで
 #pragma endregion DirectX初期化処理
 
 

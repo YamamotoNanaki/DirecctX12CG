a0da66125c2dd8e5e832e898693abc8e041296f5
虹輝 山本 <nanaki0203.y@gmail.com>
2022-04-30 22:01:50 +0900

    整理

--- GPipeline.h
+++ GPipeline.h
@@ -3,8 +3,6 @@
 
 class GPipeline
 {
-private:
-
 public:
 	D3D12_GRAPHICS_PIPELINE_STATE_DESC pipelineDesc[4] = {};
 	GPipeline(ID3DBlob* vsBlob, ID3DBlob* psBlob, D3D12_INPUT_ELEMENT_DESC* inputLayout, int layoutCount);

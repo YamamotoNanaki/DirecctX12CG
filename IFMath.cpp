b0b3af141ee09d839a56b6a9ff9456344b18f7b6
虹輝 山本 <nanaki0203.y@gmail.com>
2022-05-18 23:38:35 +0900

    Vecクラス追加

--- IFMath.cpp
+++ IFMath.cpp
@@ -16,3 +16,165 @@ Float3 IF::SetFloat3(Float2 f, float z)
 {
 	return Float3(f.x, f.y, z);
 }
+
+Float4 IF::SetFloat4(Vector3 v, float w)
+{
+	return Float4(v.x, v.y, v.z, w);
+}
+
+Float3 IF::SetFloat3(Vector3 v)
+{
+	return Float3(v.x, v.y, v.z);
+}
+
+Float2 IF::SetFloat3(Vector2 v)
+{
+	return Float2(v.x, v.y);
+}
+
+Vector3 IF::SetVector3(Float3 f)
+{
+	return Vector3(f.x, f.y, f.z);
+}
+
+Vector3 IF::SetVector3(Float2 f, float z)
+{
+	return Vector3(f.x, f.y, z);
+}
+
+Vector2 IF::SetVector2(Float2 f)
+{
+	return Vector2(f.x, f.y);
+}
+
+Matrix IF::SetMatrixX(Float4 f)
+{
+	Matrix m;
+	m._1_1 = f.x;
+	m._1_2 = f.y;
+	m._1_3 = f.z;
+	m._1_4 = f.w;
+
+	return m;
+}
+
+Matrix IF::SetMatrixY(Float4 f)
+{
+	Matrix m;
+	m._1_1 = f.x;
+	m._1_2 = f.y;
+	m._1_3 = f.z;
+	m._1_4 = f.w;
+
+	return m;
+}
+
+Matrix IF::SetMatrixZ(Float4 f)
+{
+	Matrix m;
+	m._1_1 = f.x;
+	m._1_2 = f.y;
+	m._1_3 = f.z;
+	m._1_4 = f.w;
+
+	return m;
+}
+
+Matrix IF::SetMatrixW(Float4 f)
+{
+	Matrix m;
+	m._1_1 = f.x;
+	m._1_2 = f.y;
+	m._1_3 = f.z;
+	m._1_4 = f.w;
+
+	return m;
+}
+
+Matrix IF::SetMatrixX(Float3 f, float w)
+{
+	Matrix m;
+	m._1_1 = f.x;
+	m._1_2 = f.y;
+	m._1_3 = f.z;
+	m._1_4 = w;
+
+	return m;
+}
+
+Matrix IF::SetMatrixY(Float3 f, float w)
+{
+	Matrix m;
+	m._1_1 = f.x;
+	m._1_2 = f.y;
+	m._1_3 = f.z;
+	m._1_4 = w;
+
+	return m;
+}
+
+Matrix IF::SetMatrixZ(Float3 f, float w)
+{
+	Matrix m;
+	m._1_1 = f.x;
+	m._1_2 = f.y;
+	m._1_3 = f.z;
+	m._1_4 = w;
+
+	return m;
+}
+
+Matrix IF::SetMatrixW(Float3 f, float w)
+{
+	Matrix m;
+	m._1_1 = f.x;
+	m._1_2 = f.y;
+	m._1_3 = f.z;
+	m._1_4 = w;
+
+	return m;
+}
+
+Matrix IF::SetMatrixX(Vector3 v, float w)
+{
+	Matrix m;
+	m._1_1 = v.x;
+	m._1_2 = v.y;
+	m._1_3 = v.z;
+	m._1_4 = w;
+
+	return m;
+}
+
+Matrix IF::SetMatrixY(Vector3 v, float w)
+{
+	Matrix m;
+	m._1_1 = v.x;
+	m._1_2 = v.y;
+	m._1_3 = v.z;
+	m._1_4 = w;
+
+	return m;
+}
+
+Matrix IF::SetMatrixZ(Vector3 v, float w)
+{
+	Matrix m;
+	m._1_1 = v.x;
+	m._1_2 = v.y;
+	m._1_3 = v.z;
+	m._1_4 = w;
+
+	return m;
+}
+
+Matrix IF::SetMatrixW(Vector3 v, float w)
+{
+	Matrix m;
+	m._1_1 = v.x;
+	m._1_2 = v.y;
+	m._1_3 = v.z;
+	m._1_4 = w;
+
+	return m;
+}
--- IFMath.h
+++ IFMath.h
@@ -2,12 +2,32 @@
 #include "Float2.h"
 #include "Float3.h"
 #include "Float4.h"
+#include "Vector2.h"
+#include "Vector3.h"
 #include "Matrix.h"
 
 
 namespace IF
 {
-	Float4 SetFloat4(Float3 f, float z);
-	Float4 SetFloat4(Float2 f, float z, float w);
-	Float3 SetFloat3(Float2 f, float z);
+	Float4 SetFloat4(Float3 f, float z = 0);
+	Float4 SetFloat4(Float2 f, float z = 0, float w = 0);
+	Float3 SetFloat3(Float2 f, float z = 0);
+	Float4 SetFloat4(Vector3 v, float w = 0);
+	Float3 SetFloat3(Vector3 v);
+	Float2 SetFloat3(Vector2 v);
+	Vector3 SetVector3(Float3 f);
+	Vector3 SetVector3(Float2 f, float z = 0);
+	Vector2 SetVector2(Float2 f);
+	Matrix SetMatrixX(Float4 f);
+	Matrix SetMatrixY(Float4 f);
+	Matrix SetMatrixZ(Float4 f);
+	Matrix SetMatrixW(Float4 f);
+	Matrix SetMatrixX(Float3 f, float w = 0);
+	Matrix SetMatrixY(Float3 f, float w = 0);
+	Matrix SetMatrixZ(Float3 f, float w = 0);
+	Matrix SetMatrixW(Float3 f, float w = 1);
+	Matrix SetMatrixX(Vector3 v, float w = 0);
+	Matrix SetMatrixY(Vector3 v, float w = 0);
+	Matrix SetMatrixZ(Vector3 v, float w = 0);
+	Matrix SetMatrixW(Vector3 v, float w = 1);
 };
